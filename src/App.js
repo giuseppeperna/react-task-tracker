3c581c7fe831c427a13a359be6d6695b5269546d
Giuseppe Perna <giuseppe@bluelabs.it>
2021-09-17 15:40:33 +0200

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import { useState, useEffect } from "react";
 
 function App() {
@@ -94,22 +97,31 @@ function App() {
     }
 
   return (
-    <div className="container">
-        <Header title='Task Tracker'
-                onAdd={() => setShowAddTask
-                (!showAddTask)}
-                showAdd={showAddTask}
-        />
-        { showAddTask &&
-            <AddTask onAdd={addTask}/>
-        }
-        { tasks.length > 0 ?
-            <Tasks tasks={tasks}
-                   onDelete={deleteTask}
-                   onToggle={toggleReminder}
-            />
-        : 'No Tasks' }
-    </div>
+      <Router>
+          <div className="container">
+              <Header title='Task Tracker'
+                      onAdd={() => setShowAddTask
+                      (!showAddTask)}
+                      showAdd={showAddTask}
+              />
+              <Route path='/' exact render={(props) => (
+                  <>
+                      { showAddTask &&
+                      <AddTask onAdd={addTask}/>
+                      }
+                      { tasks.length > 0 ?
+                          <Tasks tasks={tasks}
+                                 onDelete={deleteTask}
+                                 onToggle={toggleReminder}
+                          />
+                          : 'No Tasks' }
+                  </>
+              )}
+              />
+              <Route path='/about' component={About} />
+              <Footer />
+          </div>
+      </Router>
   );
 }
 
